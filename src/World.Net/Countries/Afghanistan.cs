16942574bae97601354fbae6c9dd1dba72b7550e
Raphael Anyanwu <anyanwuraphaelc@gmail.com>
2025-01-15 08:41:04 +0100

    add afgan country model

---
+++ src/World.Net/Countries/Afghanistan.cs
@@ -0,0 +1,10 @@
+﻿namespace World.Net.Countries
+{
+    public class Afghanistan : ICountry
+    {
+        public string Name { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+        public string OfficialName { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+        public int NumericCode { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+        public string CountryCode { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+    }
+}
