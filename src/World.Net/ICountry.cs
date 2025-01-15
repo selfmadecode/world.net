fe5fc400ccd62b3824f1512ce3f6e5eaa5e64a74
Raphael Anyanwu <anyanwuraphaelc@gmail.com>
2025-01-15 08:41:31 +0100

    add ICountry interface

---
+++ src/World.Net/ICountry.cs
@@ -0,0 +1,11 @@
+﻿namespace World.Net
+{
+    internal interface ICountry
+    {
+        ///<inheritdoc/>
+        string Name { get; set; }
+        string OfficialName { get; set; }
+        int NumericCode { get; set; }
+        string CountryCode { get; set; }
+    }
+}
