ed384f3cea3d2f5a3c5ad57d11f9d82aec3e270f
Raphael Anyanwu <anyanwuraphaelc@gmail.com>
2025-01-18 22:48:56 +0100

    made country identifier public

--- src/World.Net/Helpers/CountryIdentifier.cs
+++ src/World.Net/Helpers/CountryIdentifier.cs
@@ -1,6 +1,18 @@
 ﻿namespace World.Net.Helpers;
 
-internal sealed class CountryIdentifier
+/// <summary>
+/// Provides unique identifiers for countries.
+/// </summary>
+/// <remarks>
+/// This class defines constants representing unique IDs for countries
+/// </remarks>
+public sealed partial class CountryIdentifier
 {
+    /// <summary>
+    /// The unique identifier for Afghanistan.
+    /// </summary>
+    /// <remarks>
+    /// Use this identifier to reference Afghanistan in operations that require a country ID.
+    /// </remarks>
     public static readonly int AfghanistanId = 1;
 }
