4cb59d5ade4ab9602656a0194f39727a1ba58d4f
Raphael Anyanwu <anyanwuraphaelc@gmail.com>
2025-01-18 22:52:00 +0100

    add console app

---
+++ src/World.Net.Console/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+PrintSeparator("All Countries");
+
+var countries = CountryProvider.GetAllCountries();
+foreach (var country in countries)
+{
+    Console.WriteLine($"Name: {country.Name,-20} | Capital: {country.Capital}");
+}
+
+Console.WriteLine();
+PrintSeparator("Afghanistan");
+
+try
+{
+    var afghanistan = CountryProvider.GetCountry(CountryIdentifier.AfghanistanId);
+    Console.WriteLine($"Official Name: {afghanistan.OfficialName}");
+    Console.WriteLine($"Capital: {afghanistan.Capital}");
+}
+catch (CountryNotFoundException ex)
+{
+    Console.WriteLine($"Error: {ex.Message}");
+}
+
+Console.WriteLine();
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey();
+
+
+/// <summary>
+/// Prints a formatted section separator in the console.
+/// </summary>
+/// <param name="title">The title to display in the separator.</param>
+static void PrintSeparator(string title)
+{
+    Console.WriteLine(new string('-', 30));
+    Console.WriteLine($"--- {title} ---");
+    Console.WriteLine(new string('-', 30));
+    Console.WriteLine();
+}
