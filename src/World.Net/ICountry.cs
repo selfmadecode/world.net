5cdc00016cae119ddedc4ef3f7835825c8353c54
Raphael Anyanwu <anyanwuraphaelc@gmail.com>
2025-01-18 16:41:14 +0100

    added more properties and code comments in ICountry interface

--- src/World.Net/ICountry.cs
+++ src/World.Net/ICountry.cs
@@ -1,11 +1,57 @@
-﻿namespace World.Net
+﻿namespace World.Net;
+
+/// <summary>
+/// Represents a country with relevant geographical and identification information.
+/// </summary>
+public interface ICountry
 {
-    internal interface ICountry
-    {
-        ///<inheritdoc/>
-        string Name { get; set; }
-        string OfficialName { get; set; }
-        int NumericCode { get; set; }
-        string CountryCode { get; set; }
-    }
+    /// <summary>
+    /// A unique identifier for the country.
+    /// </summary>
+    int Id { get; }
+
+    /// <summary>
+    /// The common name of the country.
+    /// </summary>
+    string Name { get; }
+
+    /// <summary>
+    /// The official name of the country (if different from the common name).
+    /// </summary>
+    string OfficialName { get; }
+
+    /// <summary>
+    /// The native name of the country.
+    /// </summary>
+    string NativeName { get; }
+
+    /// <summary>
+    /// The name of the capital city of the country.
+    /// </summary>
+    string Capital { get; }
+
+    /// <summary>
+    /// The ISO 3166-1 numeric code for the country, which is a three-digit numeric code.
+    /// </summary>
+    int NumericCode { get; }
+
+    /// <summary>
+    /// The ISO 3166-1 Alpha-2 code for the country, which is a two-letter code.
+    /// </summary>
+    string ISO2Code { get; }
+
+    /// <summary>
+    /// The ISO 3166-1 Alpha-3 code for the country, which is a three-letter code.
+    /// </summary>
+    string ISO3Code { get; }
+
+    /// <summary>
+    /// The international calling code for the country.
+    /// </summary>
+    string CallingCode { get; }
+
+    /// <summary>
+    /// Gets the collection of states or administrative regions within the country.
+    /// </summary>
+    IEnumerable<State>  States { get; }
 }
