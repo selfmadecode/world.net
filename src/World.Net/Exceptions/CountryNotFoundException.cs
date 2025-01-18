5ea126652fa9be2b281fa4dbb3ebe72e06eea295
Raphael Anyanwu <anyanwuraphaelc@gmail.com>
2025-01-18 22:48:35 +0100

    add CountryNotFoundException

---
+++ src/World.Net/Exceptions/CountryNotFoundException.cs
@@ -0,0 +1,15 @@
+﻿namespace World.Net;
+
+/// <summary>
+/// The exception that is thrown when a country is not found in the collection.
+/// </summary>
+public sealed class CountryNotFoundException : Exception
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="CountryNotFoundException"/> class with a specified error message.
+    /// </summary>
+    /// <param name="message">The error message that explains the reason for the exception.</param>
+    public CountryNotFoundException(string message) : base(message)
+    {
+    }
+}
