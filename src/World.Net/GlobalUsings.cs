268a36e1908f8115bb847e3e062d3fbb97372a08
Raphael Anyanwu <anyanwuraphaelc@gmail.com>
2025-01-18 22:49:21 +0100

    update global usings

---
+++ src/World.Net/GlobalUsings.cs
@@ -0,0 +1,7 @@
+﻿global using System;
+global using System.Diagnostics.CodeAnalysis;
+global using System.Collections.Generic;
+global using System.Linq;
+global using World.Net.Countries;
+global using World.Net.Helpers;
+
