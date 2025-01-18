9a55e57a8f710f012597b64ab498f8ae78a3075a
Raphael Anyanwu <anyanwuraphaelc@gmail.com>
2025-01-18 22:50:30 +0100

    update state object add type property and set it's default value to Province

--- src/World.Net/State.cs
+++ src/World.Net/State.cs
@@ -1,28 +1,32 @@
-﻿using System;
+﻿namespace World.Net;
 
-namespace World.Net
+public class State
 {
-    internal class State
+    /// <summary>
+    /// The name of the state, province or region.
+    /// </summary>
+    public string Name { get; }
+
+    /// <summary>
+    /// The ISO 3166-2 code for the state or region.
+    /// </summary>
+    public string IsoCode { get; }
+
+    /// <summary>
+    /// The Type of state.
+    /// </summary>
+    public string Type { get; }
+
+    /// <summary>
+    /// Constructor for creating a State instance.
+    /// </summary>
+    /// <param name="name">The name of the state or region.</param>
+    /// <param name="isoCode">The ISO 3166-2 code for the state.</param>
+    /// <param name="type">The type of state.</param>
+    public State([DisallowNull] string name, [DisallowNull] string isoCode, [DisallowNull] string type = "Province")
     {
-        /// <summary>
-        /// The name of the state or region.
-        /// </summary>
-        public string Name { get; set; }
-
-        /// <summary>
-        /// The ISO 3166-2 code for the state or region.
-        /// </summary>
-        public string IsoCode { get; set; }
-
-        /// <summary>
-        /// Constructor for creating a State instance.
-        /// </summary>
-        /// <param name="name">The name of the state or region.</param>
-        /// <param name="isoCode">The ISO 3166-2 code for the state.</param>
-        public State(string name, string isoCode)
-        {
-            Name = name ?? throw new ArgumentNullException(nameof(name));
-            IsoCode = isoCode ?? throw new ArgumentNullException(nameof(isoCode));
-        }
+        Name = name;
+        IsoCode = isoCode;
+        Type = type;
     }
 }
