48beba17d007ffb511c971da6c32d09adf13bee7
Raphael Anyanwu <anyanwuraphaelc@gmail.com>
2025-01-19 01:09:50 +0100

    add unit test for fetching single country

--- src/World.Net.UnitTests/CountryProviderTest.cs
+++ src/World.Net.UnitTests/CountryProviderTest.cs
@@ -14,5 +14,28 @@ public sealed class CountryProviderTest
         Assert.NotNull(countries);
     }
 
-    
+    [Fact]
+    public void GetCountry_ShouldReturn_Country_WhenCountryExists()
+    {
+        // Arrange
+        int existingCountryId = CountryIdentifier.AfghanistanId;
+
+        // Act
+        var country = CountryProvider.GetCountry(existingCountryId);
+        
+        // Assert
+        Assert.NotNull(country);
+        Assert.NotEmpty(country.Name);
+        Assert.NotEmpty(country.States);
+        Assert.Equal(existingCountryId, country.Id);
+        Assert.Equal("Afghanistan", country.Name);
+        Assert.Equal("افغانستان", country.NativeName);
+        Assert.Equal("Islamic Republic of Afghanistan", country.OfficialName);
+        Assert.Equal(004, country.NumericCode);
+        Assert.Equal("AF", country.ISO2Code);
+        Assert.Equal("AFG", country.ISO3Code);
+        Assert.Equal("+93", country.CallingCode);
+        Assert.Equal(34, country.States.Count());
+        Assert.Equal("Kabul", country.Capital);
+    }
 }
