59d076bcaef0703489824f539d92a10c8b2f97a8
Raphael Anyanwu <anyanwuraphaelc@gmail.com>
2025-01-19 01:08:51 +0100

    add unit test for get all countries

---
+++ src/World.Net.UnitTests/CountryProviderTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using World.Net.Helpers;
+
+namespace World.Net.UnitTests;
+
+public sealed class CountryProviderTest
+{
+    [Fact]
+    public void GetAllCountries_ShouldReturn_NonEmptyAndNonNullCollection()
+    {
+        var countries = CountryProvider.GetAllCountries();
+
+        Assert.NotEmpty(countries);
+        Assert.NotNull(countries);
+    }
+
+    
+}
