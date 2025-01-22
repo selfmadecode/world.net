45b6267a9aa808c1fd43591c51c26d8e27b51024
deanology <okechukwuidam516@gmail.com>
2025-01-22 18:14:47 +0100

    Added Aland islands country details  (#2)
    * added AlandIsland country details
    
    * added unit test to validate AlandIslands details

---
+++ src/World.Net.UnitTests/Countries/AlandIslandsTest.cs
@@ -0,0 +1,25 @@
+﻿namespace World.Net.UnitTests.Countries;
+
+public sealed class AlandIslandsTest
+{
+    [Fact]
+    public void GetCountry_ReturnsCorrectInformation_ForAlandIslands()
+    {
+        // Arrange
+        int existingCountryId = CountryIdentifier.AlandIslands;
+
+        // Act
+        var country = CountryProvider.GetCountry(existingCountryId);
+
+        //Assert
+        Assert.Equal(2, country.Id);
+        Assert.Equal("AlandIslands", country.Name);
+        Assert.Equal("Åland Islands", country.OfficialName);
+        Assert.Equal("Åland", country.NativeName);
+        Assert.Equal("Mariehamn", country.Capital);
+        Assert.Equal(248, country.NumericCode);
+        Assert.Equal("AX", country.ISO2Code);
+        Assert.Equal("ALA", country.ISO3Code);
+        Assert.Equal("+358", country.CallingCode);
+    }
+}
