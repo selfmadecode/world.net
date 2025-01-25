c30bb4874e396267e4de1139d60df94e1165f9d6
deanology <okechukwuidam516@gmail.com>
2025-01-25 21:41:50 +0100

    Modified Algeria Unit test to the new approach

--- src/World.Net.UnitTests/Countries/AlgeriaTest.cs
+++ src/World.Net.UnitTests/Countries/AlgeriaTest.cs
@@ -2,6 +2,14 @@
 
 public sealed class AlgeriaTest
 {
+    private const string ALGERIA_NAME = "Algeria";
+    private const string ALGERIA_OFFICIAL_NAME = "People's Democratic Republic of Algeria";
+    private const string ALGERIA_NATIVE_NAME = "الجزائر";
+    private const string ALGERIA_CAPITAL = "Algiers";
+    private const int ALGERIA_NUMERIC_CODE = 012;
+    private const string ALGERIA_ISO2_CODE = "DZ";
+    private const string ALGERIA_ISO3_CODE = "DZA";
+    private const string ALGERIA_CALLING_CODE = "+213";
     [Fact]
     public void Algeria_HasExpectedInformation()
     {
@@ -12,15 +20,14 @@ public sealed class AlgeriaTest
         var country = CountryProvider.GetCountry(existingCountryId);
 
         //Assert
-        Assert.Equal(4, country.Id);
-        Assert.Equal("Algeria", country.Name);
-        Assert.Equal("People's Democratic Republic of Algeria", country.OfficialName);
-        Assert.Equal("الجزائر", country.NativeName);
-        Assert.Equal("Algiers", country.Capital);
-        Assert.Equal(12, country.NumericCode);
-        Assert.Equal("DZ", country.ISO2Code);
-        Assert.Equal("DZA", country.ISO3Code);
-        Assert.Equal("+213", country.CallingCode);
+        Assert.Equal(existingCountryId, country.Id);
+        Assert.Equal(ALGERIA_NAME, country.Name);
+        Assert.Equal(ALGERIA_OFFICIAL_NAME, country.OfficialName);
+        Assert.Equal(ALGERIA_NATIVE_NAME, country.NativeName);
+        Assert.Equal(ALGERIA_CAPITAL, country.Capital);
+        Assert.Equal(ALGERIA_NUMERIC_CODE, country.NumericCode);
+        Assert.Equal(ALGERIA_ISO2_CODE, country.ISO2Code);
+        Assert.Equal(ALGERIA_ISO3_CODE, country.ISO3Code);
+        Assert.Equal(ALGERIA_CALLING_CODE, country.CallingCode);
     }
-
 }
