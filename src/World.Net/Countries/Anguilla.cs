d9979ddef4ba962081eebd1ece63874465948352
Raphael Anyanwu <anyanwuraphaelc@gmail.com>
2025-01-26 14:57:47 +0100

    add anguilla country info

---
+++ src/World.Net/Countries/Anguilla.cs
@@ -0,0 +1,22 @@
+﻿namespace World.Net.Countries;
+
+internal sealed class Anguilla : ICountry
+{
+    public int Id { get; } = CountryIdentifier.Anguilla;
+
+    public string Name { get; } = nameof(Anguilla);
+
+    public string OfficialName { get; } = nameof(Anguilla);
+
+    public string NativeName { get; } = nameof(Anguilla);
+
+    public string Capital { get; } = "The Valley";
+
+    public int NumericCode { get; } = 660;
+
+    public string ISO2Code { get; } = "AI";
+
+    public string ISO3Code { get; } = "AIA";
+
+    public string CallingCode { get; } = "+1264";
+}
