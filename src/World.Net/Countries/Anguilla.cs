7d542696ebbcabf04c2b97a9b41fc6f7a01f6f19
Raphael Anyanwu <anyanwuraphaelc@gmail.com>
2025-01-26 14:58:35 +0100

    add states info for anguilla

--- src/World.Net/Countries/Anguilla.cs
+++ src/World.Net/Countries/Anguilla.cs
@@ -19,4 +19,22 @@ internal sealed class Anguilla : ICountry
     public string ISO3Code { get; } = "AIA";
 
     public string CallingCode { get; } = "+1264";
+
+    public IEnumerable<State> States =>
+    [
+        new State("Blowing Point", string.Empty, "District"),
+        new State("East End", string.Empty, "District"),
+        new State("George Hill", string.Empty, "District"),
+        new State("Island Harbour", string.Empty, "District"),
+        new State("North Hill", string.Empty, "District"),
+        new State("North Side", string.Empty, "District"),
+        new State("Sandy Ground", string.Empty, "District"),
+        new State("Sandy Hill", string.Empty, "District"),
+        new State("South Hill", string.Empty, "District"),
+        new State("Stoney Ground", string.Empty, "District"),
+        new State("The Farrington", string.Empty, "District"),
+        new State("The Quarter", string.Empty, "District"),
+        new State("The Valley", string.Empty, "District"),
+        new State("West End", string.Empty, "District")
+    ];
 }
