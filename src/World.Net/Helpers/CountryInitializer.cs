95ced880186b734fd1cc92feaafce531cec91f0c
deanology <okechukwuidam516@gmail.com>
2025-01-27 21:53:01 +0100

    resolved merge conflict

--- src/World.Net/Helpers/CountryInitializer.cs
+++ src/World.Net/Helpers/CountryInitializer.cs
@@ -8,10 +8,10 @@ internal sealed class CountryInitializer
         {
             { CountryIdentifier.AfghanistanId, new Afghanistan() },
             { CountryIdentifier.AlandIslands, new AlandIslands() },
+            { CountryIdentifier.Albania, new Albania() },
             { CountryIdentifier.Algeria, new Algeria() },
             { CountryIdentifier.AmericanSamoa, new AmericanSamoa() },
             { CountryIdentifier.Andorra, new Andorra() },
-            { CountryIdentifier.Albania, new Albania() }
             // Future countries can be added here in the same format.
         };
     }
