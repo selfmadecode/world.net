87c8d6bf5b89775a2b83d6c631814c658f3ba59b
Raphael Anyanwu <anyanwuraphaelc@gmail.com>
2025-01-29 23:37:46 +0100

    add details about antartica

---
+++ src/World.Net/Countries/Antartica.cs
@@ -0,0 +1,24 @@
+﻿namespace World.Net.Countries;
+
+internal sealed class Antartica : ICountry
+{
+    public int Id { get; } = CountryIdentifier.Antarctica;
+
+    public string Name { get; } = nameof(Antartica);
+
+    public string OfficialName { get; } = nameof(Antartica);
+
+    public string NativeName { get; } = nameof(Antartica);
+
+    public string Capital { get; } = string.Empty;
+
+    public int NumericCode { get; } = 010;
+
+    public string ISO2Code { get; } = "AQ";
+
+    public string ISO3Code { get; } = "ATA";
+
+    public string CallingCode { get; } = "+672";
+
+    public IEnumerable<State> States { get; } = [];
+}
