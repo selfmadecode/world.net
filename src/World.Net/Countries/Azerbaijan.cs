8f4890f226eddab2346b961e6d0a43a30a8a88ac
Raphael Anyanwu <anyanwuraphaelc@gmail.com>
2025-02-04 09:54:09 +0100

    add Azerbaijan properties

--- src/World.Net/Countries/Azerbaijan.cs
+++ src/World.Net/Countries/Azerbaijan.cs
@@ -1,10 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿
+namespace World.Net.Countries;
 
-namespace World.Net.Countries
+internal class Azerbaijan : ICountry
 {
-    internal class Azerbaijan
-    {
-    }
+    public int Id => CountryIdentifier.Azerbaijan;
+
+    public string Name { get; } = "Azerbaijan";
+
+    public string OfficialName { get; } = "Republic of Azerbaijan";
+
+    public string NativeName { get; } = "Azərbaycan";
+
+    public string Capital { get; } = "Baku";
+
+    public int NumericCode { get; } = 031;
+
+    public string ISO2Code { get; } = "AZ";
+
+    public string ISO3Code { get; } = "AZE";
+
+    public string CallingCode { get; } = "+994";
+
+    public IEnumerable<State> States => [
+        
+    ];
 }
