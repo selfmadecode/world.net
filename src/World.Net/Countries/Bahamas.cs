4dec5af5c433e7996c2c0480c29f9567b865e11e
Raphael Anyanwu <anyanwuraphaelc@gmail.com>
2025-02-04 15:14:28 +0100

    add The Bahamas data

---
+++ src/World.Net/Countries/Bahamas.cs
@@ -0,0 +1,25 @@
+﻿
+namespace World.Net.Countries;
+
+internal sealed class Bahamas : ICountry
+{
+    public int Id => CountryIdentifier.Bahamas;
+
+    public string Name { get; } = "The Bahamas";
+
+    public string OfficialName { get; } = "Commonwealth of the Bahamas";
+
+    public string NativeName => nameof(Bahamas);
+
+    public string Capital { get; } = "Nassau";
+
+    public int NumericCode { get; } = 044;
+
+    public string ISO2Code { get; } = "BS";
+
+    public string ISO3Code { get; } = "BHS";
+
+    public string CallingCode { get; } = "+1 242";
+
+    
+}
