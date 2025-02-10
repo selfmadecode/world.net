cc84aca17f69c2a04aa0f6bcc45e50879f115d24
Raphael Anyanwu <anyanwuraphaelc@gmail.com>
2025-02-10 21:22:44 +0100

    add Bermuda

---
+++ src/World.Net/Countries/Bermuda.cs
@@ -0,0 +1,25 @@
+﻿
+namespace World.Net.Countries;
+
+internal sealed class Bermuda : ICountry
+{
+    public int Id => throw new NotImplementedException();
+
+    public string Name => throw new NotImplementedException();
+
+    public string OfficialName => throw new NotImplementedException();
+
+    public string NativeName => throw new NotImplementedException();
+
+    public string Capital => throw new NotImplementedException();
+
+    public int NumericCode => throw new NotImplementedException();
+
+    public string ISO2Code => throw new NotImplementedException();
+
+    public string ISO3Code => throw new NotImplementedException();
+
+    public string CallingCode => throw new NotImplementedException();
+
+    public IEnumerable<State> States => throw new NotImplementedException();
+}
