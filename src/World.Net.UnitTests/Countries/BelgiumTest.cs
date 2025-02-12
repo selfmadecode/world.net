c0cf75a83146d56e070b8d717dbd4fac8e46a70e
deanology <okechukwuidam516@gmail.com>
2025-02-12 22:34:46 +0100

    Belgium belize benin (#25)
    * AlandIsland country details
    
    * added unit test to validate AlandIslands details
    
    * Added details for Algeria and Test
    
    * Algeria and American Samao country details
    
    * resolved merge conflict
    
    * Modified Algeria Unit test to the new approach
    
    * Andorra country details
    
    * PR Comments addressed
    
    * resolved merge conflict
    
    * Removed unnecessary test case
    
    * added details for Bangladesh, Barbados and Belarus
    
    * updated CountryInitializer.cs
    
    * added information for Belgium, Belize and Benin
    
    * fix unit testing mismatch
    
    * Ordered country states and remove iso-code 2 prefix on states isocodes

---
+++ src/World.Net.UnitTests/Countries/BelgiumTest.cs
@@ -0,0 +1,49 @@
+﻿namespace World.Net.UnitTests.Countries;
+
+public sealed class BelgiumTest
+{
+    private const string BELGIUM_NAME = "Belgium";
+    private const string BELGIUM_OFFICIAL_NAME = "Kingdom of Belgium";
+    private const string BELGIUM_NATIVE_NAME = "België";
+    private const string BELGIUM_CAPITAL = "Brussels";
+    private const int BELGIUM_NUMERIC_CODE = 056;
+    private const string BELGIUM_ISO2_CODE = "BE";
+    private const string BELGIUM_ISO3_CODE = "BEL";
+    private const string BELGIUM_CALLING_CODE = "+32";
+    private const int BELGIUM_REGION_COUNT = 3;
+    private const int BELGIUM_PROVINCE_COUNT = 10;
+    private const string BELGIUM_REGION_TYPE = "Region";
+    private const string BELGIUM_PROVINCE_TYPE = "Province";
+    
+    [Fact]
+    public void BelgiumTest_HasExpectedInformation()
+    {
+        // Arrange
+        int belgiumCountryId = CountryIdentifier.Belgium;
+
+        // Act
+        var country = CountryProvider.GetCountry(belgiumCountryId);
+
+        //Assert
+        // Assert
+        Assert.Equal(belgiumCountryId, country.Id);
+        Assert.Equal(BELGIUM_NAME, country.Name);
+        Assert.Equal(BELGIUM_OFFICIAL_NAME, country.OfficialName);
+        Assert.Equal(BELGIUM_NATIVE_NAME, country.NativeName);
+        Assert.Equal(BELGIUM_CAPITAL, country.Capital);
+        Assert.Equal(BELGIUM_NUMERIC_CODE, country.NumericCode);
+        Assert.Equal(BELGIUM_ISO2_CODE, country.ISO2Code);
+        Assert.Equal(BELGIUM_ISO3_CODE, country.ISO3Code);
+        Assert.Equal(BELGIUM_CALLING_CODE, country.CallingCode);
+        
+        // Validate states (regions and provinces)
+        Assert.NotNull(country.States);
+
+        // Belgium has 3 regions and 10 provinces
+        Assert.Equal(BELGIUM_REGION_COUNT + BELGIUM_PROVINCE_COUNT, country.States.Count());
+
+        // Validate region types
+        Assert.Equal(BELGIUM_REGION_COUNT, country.States.Count(s => s.Type == BELGIUM_REGION_TYPE));
+        Assert.Equal(BELGIUM_PROVINCE_COUNT, country.States.Count(s => s.Type == BELGIUM_PROVINCE_TYPE));
+    }
+}
---
+++ src/World.Net/Countries/Belgium.cs
@@ -0,0 +1,49 @@
+﻿namespace World.Net.Countries;
+
+internal sealed class Belgium : ICountry
+{
+    ///<inheritdoc/>
+    public int Id => CountryIdentifier.Belgium;
+
+    ///<inheritdoc/>
+    public string Name => nameof(Belgium);
+
+    ///<inheritdoc/>
+    public string OfficialName { get; } = "Kingdom of Belgium";
+
+    ///<inheritdoc/>
+    public string NativeName { get; } = "België";
+
+    ///<inheritdoc/>
+    public string Capital { get; } = "Brussels";
+
+    ///<inheritdoc/>
+    public int NumericCode { get; } = 056;
+
+    ///<inheritdoc/>
+    public string ISO2Code { get; } = "BE";
+
+    ///<inheritdoc/>
+    public string ISO3Code { get; } = "BEL";
+
+    ///<inheritdoc/>
+    public string CallingCode { get; } = "+32";
+
+    ///<inheritdoc/>
+    public IEnumerable<State> States { get; } =
+    [
+        new("Antwerp", "VAN"),
+        new("Brussels-Capital Region", "BRU", "Region"),
+        new("East Flanders", "VOV"),
+        new("Flemish Brabant", "VBR"),
+        new("Flemish Region", "VLG", "Region"),
+        new("Hainaut", "WHT"),
+        new("Liège", "WLG"),
+        new("Limburg", "VLI"),
+        new("Luxembourg", "WLX"),
+        new("Namur", "WNA"),
+        new("Walloon Brabant", "WBR"),
+        new("Walloon Region", "WAL", "Region"),
+        new("West Flanders", "VWV")
+    ];
+}
