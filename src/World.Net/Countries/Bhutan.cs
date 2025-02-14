fca26272a4719b88f65a7af6fc5a7c46a18038b8
Raphael Anyanwu <anyanwuraphaelc@gmail.com>
2025-02-14 08:49:31 +0100

    add Bhutan state

---
+++ src/World.Net/Countries/Bhutan.cs
@@ -0,0 +1,24 @@
+﻿namespace World.Net.Countries;
+
+internal sealed class Bhutan : ICountry
+{
+    public int Id => CountryIdentifier.Bhutan;
+
+    public string Name { get; } = "Bhutan";
+
+    public string OfficialName { get; } = "Kingdom of Bhutan";
+
+    public string NativeName { get; } = "ʼbrug-yul";
+
+    public string Capital { get; } = "Thimphu";
+
+    public int NumericCode { get; } = 064;
+
+    public string ISO2Code { get; } = "BM";
+
+    public string ISO3Code { get; } = "BMU";
+
+    public string CallingCode { get; } = "+975";
+
+    public IEnumerable<State> States => throw new NotImplementedException();
+}
