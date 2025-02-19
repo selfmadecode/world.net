14c7a6306e4513988f30b472268d6695f017a8c2
Reagan Reuben <Reagan.Reuben@Sterling.ng>
2025-02-19 11:38:04 +0100

    add details for Brazil, British Indian Ocean Territory, Brunei, Bulgaria and Burkina Faso

---
+++ src/World.Net/Countries/BritishIndianOceanTerritory.cs
@@ -0,0 +1,38 @@
+﻿namespace World.Net.Countries
+{
+    internal sealed class BritishIndianOceanTerritory : ICountry
+    {
+        ///<inheritdoc/>
+        public int Id => CountryIdentifier.BritishIndianOceanTerritory;
+
+        ///<inheritdoc/>
+        public string Name => "British Indian Ocean Territory";
+
+        ///<inheritdoc/>
+        public string OfficialName { get; } = "British Indian Ocean Territory";
+
+        ///<inheritdoc/>
+        public string NativeName { get; } = "British Indian Ocean Territory";
+
+        ///<inheritdoc/>
+        public string Capital { get; } = "Diego Garcia";
+
+        ///<inheritdoc/>
+        public int NumericCode { get; } = 086;
+
+        ///<inheritdoc/>
+        public string ISO2Code { get; } = "IO";
+
+        ///<inheritdoc/>
+        public string ISO3Code { get; } = "IOT";
+
+        ///<inheritdoc/>
+        public string CallingCode { get; } = "+246";
+
+        ///<inheritdoc/>
+        public IEnumerable<State> States { get; } =
+        [
+
+        ];
+    }
+}
