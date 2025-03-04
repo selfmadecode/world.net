e38f799ddf1ef5c376d46077ed8ca1f78a03798f
Raphael Anyanwu <anyanwuraphaelc@gmail.com>
2025-03-04 22:20:37 +0100

    add details for cayman island

---
+++ src/World.Net/Countries/CaymanIslands.cs
@@ -0,0 +1,25 @@
+﻿
+namespace World.Net.Countries;
+
+internal sealed class CaymanIslands : ICountry
+{
+    public CountryIdentifier Id => CountryIdentifier.CaymanIslands;
+
+    public string Name => "Cayman Islands";
+
+    public string OfficialName => "Cayman Islands";
+
+    public string NativeName => "Cayman Islands";
+
+    public string Capital => throw new NotImplementedException();
+
+    public int NumericCode => throw new NotImplementedException();
+
+    public string ISO2Code => throw new NotImplementedException();
+
+    public string ISO3Code => throw new NotImplementedException();
+
+    public string CallingCode => throw new NotImplementedException();
+
+    public IEnumerable<State> States => throw new NotImplementedException();
+}
