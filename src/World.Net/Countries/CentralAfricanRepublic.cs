0774479e13edbb77801e5259cc12f7e3de6a2b46
Raphael Anyanwu <anyanwuraphaelc@gmail.com>
2025-03-15 22:26:49 +0100

    add central african republic

---
+++ src/World.Net/Countries/CentralAfricanRepublic.cs
@@ -0,0 +1,40 @@
+﻿namespace World.Net.Countries;
+
+internal sealed class CentralAfricanRepublic : ICountry
+{
+    public CountryIdentifier Id => CountryIdentifier.CentralAfricanRepublic;
+
+    public string Name => "Central African Republic";
+
+    public string OfficialName => "Central African Republic";
+
+    public string NativeName => "Ködörösêse tî Bêafrîka";
+
+    public string Capital => "Bangui";
+
+    public int NumericCode => 140;
+
+    public string ISO2Code => "CF";
+
+    public string ISO3Code => "CAF";
+
+    public string CallingCode => "+236";
+
+    public IEnumerable<State> States =>
+    [
+        new("Bamingui-Bangoran", "BB", "Prefecture"),
+        new("Bangui", "BGF", "Capital District"),
+        new("Basse-Kotto", "BK", "Prefecture"),
+        new("Haut-Mbomou", "HM", "Prefecture"),
+        new("Haut-Oubangui", "HK", "Prefecture"),
+        new("Kémo", "KG", "Prefecture"),
+        new("Lobaye", "LB", "Prefecture"),
+        new("Mambéré-Kadéï", "CF", "Prefecture"),
+        new("Mbomou", "MB", "Prefecture"),
+        new("Nana-Mambéré", "NM", "Prefecture"),
+        new("Nana-Grébizi", "NG", "Prefecture"),
+        new("Ombella-M'Poko", "OM", "Prefecture"),
+        new("Sangha-Mbaéré", "SM", "Prefecture"),
+        new("Vakaga", "VK", "Prefecture")
+    ];
+}
