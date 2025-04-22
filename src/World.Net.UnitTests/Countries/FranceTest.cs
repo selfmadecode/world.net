f7ada3973db377176faf6caac1194d5b072f6ca4
ReaganDev <charixma95@gmail.com>
2025-04-22 12:50:07 +0100

    add country details for France, FrenchGuiana and FrenchPolynesia

---
+++ src/World.Net.UnitTests/Countries/FranceTest.cs
@@ -0,0 +1,37 @@
+﻿namespace World.Net.UnitTests.Countries;
+public sealed class FranceTest
+{
+    private const string FRANCE_NAME = "France";
+    private const int FRANCE_STATE_COUNT = 116;
+    private const string FRANCE_OFFICIAL_NAME = "French Republic";
+    private const string FRANCE_NATIVE_NAME = "France";
+    private const string FRANCE_CAPITAL = "Paris";
+    private const int FRANCE_NUMERIC_CODE = 250;
+    private const string FRANCE_ISO2_CODE = "FR";
+    private const string FRANCE_ISO3_CODE = "FRA";
+    private readonly string[] FRANCE_CALLING_CODE = ["+33"];
+
+
+    [Fact]
+    public void GetCountry_ReturnsCorrectInformation_ForFrance()
+    {
+        // Arrange
+        CountryIdentifier existingCountryId = CountryIdentifier.France;
+
+        // Act
+        var country = CountryProvider.GetCountry(existingCountryId);
+
+        //Assert
+        Assert.Equal(existingCountryId, country.Id);
+        Assert.Equal(FRANCE_NAME, country.Name);
+        Assert.NotNull(country.States);
+        Assert.Equal(FRANCE_STATE_COUNT, country.States.Count());
+        Assert.Equal(FRANCE_OFFICIAL_NAME, country.OfficialName);
+        Assert.Equal(FRANCE_NATIVE_NAME, country.NativeName);
+        Assert.Equal(FRANCE_CAPITAL, country.Capital);
+        Assert.Equal(FRANCE_NUMERIC_CODE, country.NumericCode);
+        Assert.Equal(FRANCE_ISO2_CODE, country.ISO2Code);
+        Assert.Equal(FRANCE_ISO3_CODE, country.ISO3Code);
+        Assert.Equal(FRANCE_CALLING_CODE, country.CallingCode);
+    }
+}
---
+++ src/World.Net/Countries/FrenchPolynesia.cs
@@ -0,0 +1,40 @@
+﻿namespace World.Net.Countries;
+internal sealed class FrenchPolynesia : ICountry
+{
+    //<inheritdoc/>
+    public CountryIdentifier Id => CountryIdentifier.FrenchPolynesia;
+
+    //<inheritdoc/>
+    public string Name { get; } = "French Polynesia";
+
+    //<inheritdoc/>
+    public string OfficialName { get; } = "Polynésie française";
+
+    //<inheritdoc/>
+    public string NativeName => "Polynésie française";
+
+    //<inheritdoc/>
+    public string Capital { get; } = "Papeete";
+
+    //<inheritdoc/>
+    public int NumericCode { get; } = 258;
+
+    //<inheritdoc/>
+    public string ISO2Code { get; } = "PF";
+
+    //<inheritdoc/>
+    public string ISO3Code { get; } = "PYF";
+
+    //<inheritdoc/>
+    public string[] CallingCode { get; } = ["+689"];
+
+    //<inheritdoc/>
+    public IEnumerable<State> States =>
+    [
+        new("N'Austral Islands", "PF-01", "division"),
+        new("Leeward Islands", "PF-02", "division"),
+        new("Marquesas Islands", "PF-03", "division"),
+        new("Tuamotu-Gambier", "PF-04", "division"),
+        new("Windward Islands", "PF-05", "division"),
+    ];
+}
