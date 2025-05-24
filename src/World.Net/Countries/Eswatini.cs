934a0eab5681175b06a5bdf077ab8d84749f29b8
Raphael Anyanwu <53215747+selfmadecode@users.noreply.github.com>
2025-05-24 14:26:27 +0100

    add country details for Estonia Ethiopia FalklandIslands and Eswatini (#57)
    * add estonia
    
    * initialize estonia
    
    * add Ethiopia details
    
    * rename estonia test class
    
    * add ethiopia test class
    
    * add FalklandIslands details
    
    * add FalklandIslands tests
    
    * add Eswatini
    
    * add Eswatini test class

---
+++ src/World.Net/Countries/Eswatini.cs
@@ -0,0 +1,32 @@
+﻿namespace World.Net.Countries
+{
+    internal sealed class Eswatini : ICountry
+    {
+        public CountryIdentifier Id => CountryIdentifier.Eswatini;
+
+        public string Name => nameof(Eswatini);
+
+        public string OfficialName => "Kingdom of Eswatini";
+
+        public string NativeName => "eSwatini";
+
+        public string Capital => "Mbabane";
+
+        public int NumericCode => 748;
+
+        public string ISO2Code => "SZ";
+
+        public string ISO3Code => "SWZ";
+
+        public string[] CallingCode => ["268"];
+
+        public IEnumerable<State> States =>
+        [
+            new State("Hhohho", "HH", "Region"),
+            new State("Lubombo", "LU", "Region"),
+            new State("Manzini", "MA", "Region"),
+            new State("Shiselweni", "SH", "Region")
+        ];
+    }
+
+}
