e57de560b53d40d894637b901c9d57d329f2668a
Raphael Anyanwu <anyanwuraphaelc@gmail.com>
2025-05-24 11:37:23 +0100

    add estonia

---
+++ src/World.Net.UnitTests/Countries/EstoniaTests.cs
@@ -0,0 +1,40 @@
+﻿namespace World.Net.UnitTests.Countries
+{
+    public sealed class EstoniaTests
+    {
+        private const string ESTONIA_NAME = "Estonia";
+        private const int ESTONIA_STATE_COUNT = 15;
+        private const string ESTONIA_OFFICIAL_NAME = "Republic of Estonia";
+        private const string ESTONIA_NATIVE_NAME = "Eesti";
+        private const string ESTONIA_CAPITAL = "Tallinn";
+        private const int ESTONIA_NUMERIC_CODE = 233;
+        private const string ESTONIA_ISO2_CODE = "EE";
+        private const string ESTONIA_ISO3_CODE = "EST";
+        private readonly string[] ESTONIA_CALLING_CODE = ["372"];
+        private static readonly string[] VALID_STATE_TYPES = { "County" };
+
+        [Fact]
+        public void GetCountry_ReturnsCorrectInformation_ForEstonia()
+        {
+            // Arrange
+            CountryIdentifier existingCountryId = CountryIdentifier.Estonia;
+
+            // Act
+            var country = CountryProvider.GetCountry(existingCountryId);
+
+            // Assert
+            Assert.Equal(existingCountryId, country.Id);
+            Assert.Equal(ESTONIA_NAME, country.Name);
+            Assert.NotNull(country.States);
+            Assert.Equal(ESTONIA_STATE_COUNT, country.States.Count());
+            Assert.Equal(ESTONIA_OFFICIAL_NAME, country.OfficialName);
+            Assert.Equal(ESTONIA_NATIVE_NAME, country.NativeName);
+            Assert.Equal(ESTONIA_CAPITAL, country.Capital);
+            Assert.Equal(ESTONIA_NUMERIC_CODE, country.NumericCode);
+            Assert.Equal(ESTONIA_ISO2_CODE, country.ISO2Code);
+            Assert.Equal(ESTONIA_ISO3_CODE, country.ISO3Code);
+            Assert.All(country.States, state => Assert.Contains(state.Type, VALID_STATE_TYPES));
+            Assert.Equal(ESTONIA_CALLING_CODE, country.CallingCode);
+        }
+    }
+}
---
+++ src/World.Net/Countries/Estonia.cs
@@ -0,0 +1,43 @@
+﻿
+namespace World.Net.Countries
+{
+    internal class Estonia : ICountry
+    {
+        public CountryIdentifier Id => CountryIdentifier.Estonia;
+
+        public string Name => nameof(Estonia);
+
+        public string OfficialName => "Republic of Estonia";
+
+        public string NativeName => "Eesti";
+
+        public string Capital => "Tallinn";
+
+        public int NumericCode => 233;
+
+        public string ISO2Code => "EE";
+
+        public string ISO3Code => "EST";
+
+        public string[] CallingCode => ["372"];
+
+        public IEnumerable<State> States =>
+        [
+            new("Harju", "37", "County"),
+            new("Hiiu", "39", "County"),
+            new("Ida-Viru", "44", "County"),
+            new("Jõgeva", "49", "County"),
+            new("Järva", "51", "County"),
+            new("Lääne", "57", "County"),
+            new("Lääne-Viru", "59", "County"),
+            new("Põlva", "65", "County"),
+            new("Pärnu", "67", "County"),
+            new("Rapla", "70", "County"),
+            new("Saare", "74", "County"),
+            new("Tartu", "78", "County"),
+            new("Valga", "82", "County"),
+            new("Viljandi", "84", "County"),
+            new("Võru", "86", "County")
+        ];
+    }
+}
