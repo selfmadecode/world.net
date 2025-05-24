d8c4952bd6811b78c8d914fda4a407313acee4da
Raphael Anyanwu <53215747+selfmadecode@users.noreply.github.com>
2025-05-24 18:37:29 +0100

    add country details for ElSalvador EquatorialGuinea and Eritrea (#58)
    * add estonia
    
    * initialize estonia
    
    * add Ethiopia details
    
    * rename estonia test class
    
    * add ethiopia test class
    
    * add FalklandIslands details
    
    * add FalklandIslands tests
    
    * add Eswatini
    
    * add Eswatini test class
    
    * add el salvador
    
    * add equatorial
    
    * add eritrea
    
    * update initializer
    
    * remove duplicate keys

---
+++ src/World.Net/Countries/ElSalvador.cs
@@ -0,0 +1,41 @@
+﻿namespace World.Net.Countries
+{
+    internal class ElSalvador : ICountry
+    {
+        public CountryIdentifier Id => CountryIdentifier.ElSalvador;
+
+        public string Name => nameof(ElSalvador);
+
+        public string OfficialName => "Republic of El Salvador";
+
+        public string NativeName => "República de El Salvador";
+
+        public string Capital => "San Salvador";
+
+        public int NumericCode => 222;
+
+        public string ISO2Code => "SV";
+
+        public string ISO3Code => "SLV";
+
+        public string[] CallingCode => ["503"];
+
+        public IEnumerable<State> States =>
+        [
+            new("Ahuachapán", "AH", "Department"),
+            new("Cabañas", "CA", "Department"),
+            new("Chalatenango", "CH", "Department"),
+            new("Cuscatlán", "CU", "Department"),
+            new("La Libertad", "LI", "Department"),
+            new("La Paz", "PA", "Department"),
+            new("La Unión", "UN", "Department"),
+            new("Morazán", "MO", "Department"),
+            new("San Miguel", "SM", "Department"),
+            new("San Salvador", "SS", "Department"),
+            new("San Vicente", "SV", "Department"),
+            new("Santa Ana", "SA", "Department"),
+            new("Sonsonate", "SO", "Department"),
+            new("Usulután", "US", "Department")
+        ];
+    }
+}
---
+++ src/World.Net/Countries/EquatorialGuinea.cs
@@ -0,0 +1,36 @@
+﻿namespace World.Net.Countries
+{
+    internal sealed class EquatorialGuinea : ICountry
+    {
+        public CountryIdentifier Id => CountryIdentifier.EquatorialGuinea;
+
+        public string Name => nameof(EquatorialGuinea);
+
+        public string OfficialName => "Republic of Equatorial Guinea";
+
+        public string NativeName => "República de Guinea Ecuatorial";
+
+        public string Capital => "Malabo";
+
+        public int NumericCode => 226;
+
+        public string ISO2Code => "GQ";
+
+        public string ISO3Code => "GNQ";
+
+        public string[] CallingCode => new[] { "240" };
+
+        public IEnumerable<State> States =>
+        [
+            new("Annobón", "AN", "Province"),
+            new("Bioko Norte", "BN", "Province"),
+            new("Bioko Sur", "BS", "Province"),
+            new("Centro Sur", "CS", "Province"),
+            new("Djibloho", "DJ", "Province"),
+            new("Kié-Ntem", "KN", "Province"),
+            new("Litoral", "LI", "Province"),
+            new("Wele-Nzas", "WN", "Province")
+        ];
+    }
+
+}
