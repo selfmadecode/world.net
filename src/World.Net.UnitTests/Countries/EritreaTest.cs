ee912f3d3a24c9ebf30461dfc49b7c3d97ac9d19
Raphael Anyanwu <anyanwuraphaelc@gmail.com>
2025-05-24 17:53:30 +0100

    add eritrea

---
+++ src/World.Net.UnitTests/Countries/EritreaTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace World.Net.UnitTests.Countries
+{
+    public class EritreaTest
+    {
+        private const string ERITREA_NAME = "Eritrea";
+        private const int ERITREA_STATE_COUNT = 6;
+        private const string ERITREA_OFFICIAL_NAME = "State of Eritrea";
+        private const string ERITREA_NATIVE_NAME = "ሃገረ ኤርትራ";
+        private const string ERITREA_CAPITAL = "Asmara";
+        private const int ERITREA_NUMERIC_CODE = 232;
+        private const string ERITREA_ISO2_CODE = "ER";
+        private const string ERITREA_ISO3_CODE = "ERI";
+        private readonly string[] ERITREA_CALLING_CODE = ["291"];
+        private static readonly string[] VALID_STATE_TYPES = { "Region" };
+
+        [Fact]
+        public void GetCountry_ReturnsCorrectInformation_ForEritrea()
+        {
+            // Arrange
+            CountryIdentifier existingCountryId = CountryIdentifier.Eritrea;
+
+            // Act
+            var country = CountryProvider.GetCountry(existingCountryId);
+
+            // Assert
+            Assert.Equal(existingCountryId, country.Id);
+            Assert.Equal(ERITREA_NAME, country.Name);
+            Assert.NotNull(country.States);
+            Assert.Equal(ERITREA_STATE_COUNT, country.States.Count());
+            Assert.Equal(ERITREA_OFFICIAL_NAME, country.OfficialName);
+            Assert.Equal(ERITREA_NATIVE_NAME, country.NativeName);
+            Assert.Equal(ERITREA_CAPITAL, country.Capital);
+            Assert.Equal(ERITREA_NUMERIC_CODE, country.NumericCode);
+            Assert.Equal(ERITREA_ISO2_CODE, country.ISO2Code);
+            Assert.Equal(ERITREA_ISO3_CODE, country.ISO3Code);
+            Assert.All(country.States, state => Assert.Contains(state.Type, VALID_STATE_TYPES));
+            Assert.Equal(ERITREA_CALLING_CODE, country.CallingCode);
+        }
+    }
+}
---
+++ src/World.Net/Countries/Eritrea.cs
@@ -0,0 +1,34 @@
+﻿namespace World.Net.Countries
+{
+    internal sealed class Eritrea : ICountry
+    {
+        public CountryIdentifier Id => CountryIdentifier.Eritrea;
+
+        public string Name => nameof(Eritrea);
+
+        public string OfficialName => "State of Eritrea";
+
+        public string NativeName => "ሃገረ ኤርትራ"; // Tigrinya: Hagere Ertra
+
+        public string Capital => "Asmara";
+
+        public int NumericCode => 232;
+
+        public string ISO2Code => "ER";
+
+        public string ISO3Code => "ERI";
+
+        public string[] CallingCode => ["291"];
+
+        public IEnumerable<State> States =>
+        [
+            new("Anseba", "AN", "Region"),
+            new("Debub", "DU", "Region"),
+            new("Debubawi Keyih Bahri", "DK", "Region"),
+            new("Gash-Barka", "GB", "Region"),
+            new("Maekel", "MA", "Region"),
+            new("Semenawi Keyih Bahri", "SK", "Region")
+        ];
+    }
+
+}
