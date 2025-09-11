9807581fb3f215a4de04e868235efab1e8181940
Raphael Anyanwu <anyanwuraphaelc@gmail.com>
2025-09-11 09:32:20 +0100

    initialize kenya

--- src/World.Net/Helpers/CountryInitializer.cs
+++ src/World.Net/Helpers/CountryInitializer.cs
@@ -92,7 +92,8 @@ internal sealed class CountryInitializer
             { CountryIdentifier.Japan, new Japan() },
             { CountryIdentifier.Jersey, new Jersey() },
             { CountryIdentifier.Jordan, new Jordan() },
-            { CountryIdentifier.Kazakhstan, new Kazakhstan() }
+            { CountryIdentifier.Kazakhstan, new Kazakhstan() },
+            { CountryIdentifier.Kenya, new Kenya() },
 
             // Future countries can be added here in the same format.
         };
