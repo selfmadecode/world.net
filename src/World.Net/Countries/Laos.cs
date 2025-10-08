c43d80e35986e56af277efbd80248e9e16412208
Raphael Anyanwu <anyanwuraphaelc@gmail.com>
2025-10-09 00:10:58 +0100

    add full details for Laos

--- src/World.Net/Countries/Laos.cs
+++ src/World.Net/Countries/Laos.cs
@@ -1,6 +1,55 @@
 ﻿namespace World.Net.Countries
 {
-    public sealed class Laos : ICountry
+    internal sealed class Laos : ICountry
     {
+        ///<inheritdoc/>
+        public CountryIdentifier Id => CountryIdentifier.Laos;
+
+        ///<inheritdoc/>
+        public string Name { get; } = "Laos";
+
+        ///<inheritdoc/>
+        public string OfficialName { get; } = "Lao People's Democratic Republic";
+
+        ///<inheritdoc/>
+        public string NativeName { get; } = "ສາທາລະນະລັດ ປະຊາຊົນລາວ";
+
+        ///<inheritdoc/>
+        public string Capital { get; } = "Vientiane";
+
+        ///<inheritdoc/>
+        public int NumericCode { get; } = 418;
+
+        ///<inheritdoc/>
+        public string ISO2Code { get; } = "LA";
+
+        ///<inheritdoc/>
+        public string ISO3Code { get; } = "LAO";
+
+        ///<inheritdoc/>
+        public string[] CallingCode { get; } = ["+856"];
+
+        public IEnumerable<State> States { get; } =
+        [
+            new("Attapeu", "LA-AT"),
+            new("Bokeo", "LA-BK"),
+            new("Bolikhamxai", "LA-BL"),
+            new("Champasak", "LA-CH"),
+            new("Houaphan", "LA-HO"),
+            new("Khammouan", "LA-KH"),
+            new("Luang Namtha", "LA-LM"),
+            new("Luang Prabang", "LA-LP"),
+            new("Oudomxai", "LA-OU"),
+            new("Phongsaly", "LA-PH"),
+            new("Salavan", "LA-SL"),
+            new("Savannakhét", "LA-SV"),
+            new("Sekong", "LA-XE"),
+            new("Vientiane Province", "LA-VI"),
+            new("Vientiane Prefecture", "LA-VT", "Prefecture"),
+            new("Xaignabouli", "LA-XA"),
+            new("Xaisomboun", "LA-XN"),
+            new("Xiangkhouang", "LA-XI")
+        ];
     }
+
 }
