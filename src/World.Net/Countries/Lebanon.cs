02d22743f40a8ccd01638fcb1b61b7062478d3af
Raphael Anyanwu <anyanwuraphaelc@gmail.com>
2025-10-09 22:17:20 +0100

    add lebanon state details

---
+++ src/World.Net/Countries/Lebanon.cs
@@ -0,0 +1,46 @@
+﻿namespace World.Net.Countries
+{
+    internal sealed class Lebanon : ICountry
+    {
+        //<inheritdoc/>
+        public CountryIdentifier Id => CountryIdentifier.Lebanon;
+
+        //<inheritdoc/>
+        public string Name { get; } = "Lebanon";
+
+        //<inheritdoc/>
+        public string OfficialName { get; } = "Lebanese Republic";
+
+        //<inheritdoc/>
+        public string NativeName { get; } = "الجمهورية اللبنانية";
+
+        //<inheritdoc/>
+        public string Capital { get; } = "Beirut";
+
+        //<inheritdoc/>
+        public int NumericCode { get; } = 422;
+
+        //<inheritdoc/>
+        public string ISO2Code { get; } = "LB";
+
+        //<inheritdoc/>
+        public string ISO3Code { get; } = "LBN";
+
+        //<inheritdoc/>
+        public string[] CallingCode { get; } = ["+961"];
+
+        //<inheritdoc/>
+        public IEnumerable<State> States { get; } =
+        [
+            new("Akkar", "LB-AK", "Governorate"),
+            new("Baalbek-Hermel", "LB-BH", "Governorate"),
+            new("Beirut", "LB-BA", "Governorate"),
+            new("Beqaa", "LB-BI", "Governorate"),
+            new("Mount Lebanon", "LB-JL", "Governorate"),
+            new("Nabatieh", "LB-NA", "Governorate"),
+            new("North Lebanon", "LB-AS", "Governorate"),
+            new("South Lebanon", "LB-JA", "Governorate")
+        ];
+    }
+
+}
