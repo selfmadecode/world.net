3f87a578ade550ab2481668357059cccf8d66a4e
Raphael Anyanwu <anyanwuraphaelc@gmail.com>
2025-10-09 20:00:42 +0100

    initalize latvia

--- src/World.Net/Helpers/CountryInitializer.cs
+++ src/World.Net/Helpers/CountryInitializer.cs
@@ -99,6 +99,7 @@ internal sealed class CountryInitializer
             { CountryIdentifier.Kuwait, new Kuwait() },
             { CountryIdentifier.Kyrgyzstan, new Kyrgyzstan() },
             { CountryIdentifier.Laos, new Laos() },
+            { CountryIdentifier.Latvia, new Latvia() },
 
             // Future countries can be added here in the same format.
         };
