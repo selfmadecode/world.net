f3c10860532aa3c29c138bc5a5640faf4b2de9e3
Raphael Anyanwu <anyanwuraphaelc@gmail.com>
2025-10-15 22:26:49 +0100

    update Laos tests

--- src/World.Net.UnitTests/Countries/LaosTest.cs
+++ src/World.Net.UnitTests/Countries/LaosTest.cs
@@ -1,6 +1,6 @@
 ﻿namespace World.Net.UnitTests.Countries
 {
-    public class LaosTest
+    public class LaosTest : AssertCountryTestBase
     {
         private const CountryIdentifier ExpectedId = CountryIdentifier.Laos;
         private const string ExpectedName = "Laos";
@@ -38,24 +38,20 @@
         public void GetCountry_ReturnsCorrectInformation_ForLaos()
         {
             var country = CountryProvider.GetCountry(ExpectedId);
-            Assert.Equal(ExpectedId, country.Id);
-            Assert.Equal(ExpectedName, country.Name);
-            Assert.Equal(ExpectedOfficialName, country.OfficialName);
-            Assert.Equal(ExpectedNativeName, country.NativeName);
-            Assert.Equal(ExpectedCapital, country.Capital);
-            Assert.Equal(ExpectedNumericCode, country.NumericCode);
-            Assert.Equal(ExpectedISO2Code, country.ISO2Code);
-            Assert.Equal(ExpectedISO3Code, country.ISO3Code);
-            Assert.Equal(ExpectedCallingCode, country.CallingCode);
 
-            var states = country.States.ToArray();
-            Assert.Equal(ExpectedStates.Length, states.Length);
-            for (int i = 0; i < states.Length; i++)
-            {
-                Assert.Equal(ExpectedStates[i].Name, states[i].Name);
-                Assert.Equal(ExpectedStates[i].IsoCode, states[i].IsoCode);
-                Assert.Equal(ExpectedStates[i].Type, states[i].Type);
-            }
+            AssertCorrectInformation(
+                country,
+                ExpectedId,
+                ExpectedName,
+                ExpectedOfficialName,
+                ExpectedNativeName,
+                ExpectedCapital,
+                ExpectedNumericCode,
+                ExpectedISO2Code,
+                ExpectedISO3Code,
+                ExpectedCallingCode,
+                ExpectedStates
+            );
         }
     }
 }
